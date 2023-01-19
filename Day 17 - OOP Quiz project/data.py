fa6973c0ce5e92397cbab9fff23f06511fb753bc
Kim <101264719+MonaBlanc@users.noreply.github.com>
2023-01-19 14:10:18 +0100

    Day Seventeen completed
    V2 with API usage

---
+++ Day 17 - OOP Quiz project/data.py
@@ -0,0 +1,82 @@
+question_data = [
+        {
+            "category":"Entertainment: Japanese Anime & Manga",
+            "type":"boolean",
+            "difficulty":"easy",
+            "question":"In the 1988 film &quot;Akira&quot;, Tetsuo ends up destroying Tokyo.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        },
+        {
+            "category":"Entertainment: Japanese Anime & Manga",
+            "type":"boolean",
+            "difficulty":"medium",
+            "question":"The animated film &quot;Spirited Away&quot; won the Academy Award for Best Animated Feature at the 75th Academy Awards in 2003.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        },
+        {
+            "category":"Entertainment: Japanese Anime & Manga",
+            "type":"boolean",
+            "difficulty":"easy",
+            "question":"In Kill La Kill, the weapon of the main protagonist is a katana. ",
+            "correct_answer":"False",
+            "incorrect_answers":["True"]
+        },
+        {
+            "category":"Entertainment: Japanese Anime & Manga",
+            "type":"boolean",
+            "difficulty":"easy",
+            "question":"In the &quot;Melancholy of Haruhi Suzumiya&quot; series, the narrator goes by the nickname Kyon.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        },
+        {
+            "category":"Entertainment: Japanese Anime & Manga",
+            "type":"boolean",
+            "difficulty":"medium",
+            "question":"The anime Attack on Titan was directed by Tetsur\u014d Araki, the same person who directed the anime Highschool of the Dead.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        },
+        {
+            "category":"Entertainment: Japanese Anime & Manga",
+            "type":"boolean",
+            "difficulty":"easy",
+            "question":"In Pok&eacute;mon, Ash&#039;s Pikachu refuses to go into a pokeball.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        },
+        {
+            "category":"Entertainment: Japanese Anime & Manga",
+            "type":"boolean",
+            "difficulty":"easy",
+            "question":"The name of the attack &quot;Kamehameha&quot; in Dragon Ball Z was named after a famous king of Hawaii.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        },
+        {
+            "category":"Entertainment: Japanese Anime & Manga",
+            "type":"boolean",
+            "difficulty":"easy",
+            "question":"Kiznaiver is an adaptation of a manga.",
+            "correct_answer":"False",
+            "incorrect_answers":["True"]
+        },
+        {
+            "category":"Entertainment: Japanese Anime & Manga",
+            "type":"boolean",
+            "difficulty":"hard",
+            "question":"In the &quot;Kagerou Daze&quot; series, Shintaro Kisaragi is prominently shown with the color red.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        },
+        {
+            "category":"Entertainment: Japanese Anime & Manga",
+            "type":"boolean",
+            "difficulty":"medium",
+            "question":"In Full Metal Panic!, Whispered are those who are capable of creating Black Technology.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        }
+]
---
+++ Day 17 - OOP Quiz project/question_model.py
@@ -0,0 +1,6 @@
+#Quiz question model 
+
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
